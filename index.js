afe39cc65e2273ea0f0dce041df5040f983864a3
vrushali-d <vrushali.divekar98@gmail.com>
2023-07-08 18:54:10 +0530

    v1-Added links for code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let projects = [
         id:1,
         title:'Random Problem Selector',
         info:'This program using codeforces APIs to select a random problem to solve from codeforces website. We can also apply the filters to select the difficulty of problem and type of problem',
-        codeLink:'',
+        codeLink:'https://github.com/vrushali-d/vrushali-d.github.io/tree/master/RandomProblem',
         demoLink:'./RandomProblem/index.html',
 
     },
@@ -14,14 +14,14 @@ let projects = [
         id:2,
         title:'Rotate Square',
         info:'This program shows how we can rotate square. There is controller which we can use to rotate square by some angle. Rotation matrix is used in this program',
-        codeLink:'',
+        codeLink:'https://github.com/vrushali-d/vrushali-d.github.io/tree/master/RotateSquare',
         demoLink:'./RotateSquare/RotateSquare.html',
     },
     {
         id:3,
         title:'Flexbox Playground',
         info:'There is flexbox layout in CSS.This program shows how we can align items using different flexbox properties',
-        codeLink:'',
+        codeLink:'https://github.com/vrushali-d/vrushali-d.github.io/tree/master/AlignCSSFlexContainer',
         demoLink:'./AlignCSSFlexContainer/index.html',
     }
 ];
