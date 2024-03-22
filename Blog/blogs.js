90c184a2cee0be09aaec25f347f69f7e188bacf0
vrushali-d <vrushali.divekar98@gmail.com>
2024-03-22 20:12:59 +0530

    added blog section

---
+++ Blog/blogs.js
@@ -0,0 +1,11 @@
+const posts = [{id:1,title:"Importance of English",content:`Until I was pursuing my master's degree, I considered English to be a mere formality—a fancy skill that people should know to make a good impression. I assumed I would secure a job in my hometown, surrounded by colleagues who spoke our native language exclusively.
+
+Upon landing my job, my boss emphasized the immediate need to enhance my "English speaking skills" and "formal email writing." This was because many colleagues were comfortable communicating in English, and some even worked from the US/Canada, where our clients were also located.
+
+Lesson learned: Possessing strong English communication skills is a fundamental requirement. Anyone working with businesses that have international clients or employees from diverse regions, including India, should be capable of expressing ideas clearly and concisely in English.
+
+It's fortunate that I learned this lesson; having good English communication skills opens doors to more opportunities.
+
+P.S.: This principle applies not just to English but to proficiency in other languages as well.
+`}];
+
